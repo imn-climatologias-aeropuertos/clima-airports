66b1fa18cd98510a676c0e7e03034dc67fc264af
diego-garro <diego.garromolina@yahoo.com>
2023-08-23 15:15:46 -0600

    modify tests for Icao model

--- tests/test_icao.py
+++ tests/test_icao.py
@@ -1,6 +1,23 @@
+
+import pytest
+from pydantic import ValidationError
+
 from src.models import Icao
 
 def test_icao_is_alphanumeric():
-    icao = Icao(code="mroc")
-    icao = Icao(code="mr42")
-    icao = Icao(code="MRPV")
+    mroc = Icao(code="mroc")
+    assert isinstance(mroc, Icao)
+    
+    mr42 = Icao(code="mr42")
+    assert isinstance(mr42, Icao)
+    
+    mrpv = Icao(code="MRPV")
+    assert isinstance(mrpv, Icao)
+
+def test_icao_is_not_alphanumeric():
+    bad_icaos = ["mro!", "/kd2", "1234", ".pro"]
+    
+    for icao in bad_icaos:
+        with pytest.raises(ValidationError) as exc_info:
+            bad = Icao(code=icao)
+            assert isinstance(bad, Icao)
