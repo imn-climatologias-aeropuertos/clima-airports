f92d11b3265389ff5db034e606ea665cccb6b49c
diego-garro <diego.garromolina@yahoo.com>
2023-08-28 11:38:49 -0600

    add queries module

---
+++ src/queries.py
@@ -0,0 +1,77 @@
+from typing import Optional
+
+import pandas as pd
+
+columns = [
+    "date",
+    "year",
+    "month",
+    "day",
+    "hour",
+    "minute",
+    "wind_direction",
+    "wind_speed",
+    "wind_gust",
+    "visibility",
+    "cavok",
+    "weather_intensity",
+    "weather_description",
+    "weather_precipitation",
+    "weather_obscuration",
+    "sky_layer1_cover",
+    "sky_layer1_height",
+    "sky_layer1_cloud",
+    "sky_layer2_cover",
+    "sky_layer2_height",
+    "sky_layer2_cloud",
+    "sky_layer3_cover",
+    "sky_layer3_height",
+    "sky_layer3_cloud",
+    "sky_layer4_cover",
+    "sky_layer4_height",
+    "sky_layer4_cloud",
+    "temperature",
+    "dewpoint",
+    "pressure",
+]
+
+
+def read_metars_csv(
+    station_name: str = "mroc",
+    filepath: Optional[str] = None,
+) -> pd.DataFrame:
+    """Reads a METAR's CSV file and sets the 'date' column as the index.
+
+    Args:
+        station_name (str)
+        filename (str, optional):  Defaults to "metars.csv".
+
+    Returns:
+        pd.DataFrame
+    """
+    if filepath:
+        df = pd.read_csv(filepath, parse_dates=["date"], usecols=columns)
+    else:
+        df = pd.read_csv(
+            f"./data/{station_name}/metars.csv", parse_dates=["date"], usecols=columns
+        )
+    df = df.set_index(["date"])
+
+    return df
+
+
+def by_year(df: pd.DataFrame, start: int, end: Optional[int] = None) -> pd.DataFrame:
+    """Returns a DataFrame selected by the `start` and `end` (if provided, excluded) years.
+
+    Args:
+        df (pd.DataFrame): The data
+        start (int).
+        end (Optional[int], optional): Defaults to None.
+
+    Returns:
+        pd.DataFrame
+    """
+    if end is None:
+        return df.query(f"index.dt.year == {start}")
+
+    return df.query(f"index.dt.year >= {start} and index.dt.year < {end}")
---
+++ tests/test_queries.py
@@ -0,0 +1,25 @@
+import pytest
+import numpy as np
+
+import src.queries as queries
+
+
+@pytest.fixture
+def data():
+    return queries.read_metars_csv(filepath="./tests/mocks/data/metars.csv")
+
+
+def test_by_year_unique(data):
+    df = queries.by_year(data, 2009)
+    result = df["year"].unique()
+    expected = np.array([2009])
+
+    assert np.array_equal(result, expected)
+
+
+def test_by_year_range(data):
+    df = queries.by_year(data, 2005, end=2020)
+    result = df["year"].unique()
+    expected = np.array([2005, 2009, 2014, 2018])
+
+    assert np.array_equal(result, expected)
